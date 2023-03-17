927df43cc1d1059f9bb16191d3035a4e72ae85ef
Marios Akritas <marios_akritas@yahoo.com>
2023-03-17 16:18:25 +0200

    progress in pipeline code + granger cauality example

--- models/MA_dw_to_google_pipeline.py
+++ models/MA_dw_to_google_pipeline.py
@@ -123,27 +123,51 @@ def get_interest_over_time(keyword, start_date = '2019-01-01', end_date=f'{date.
 
     return google_df
 
+def get_dw_timeseries(df_clean, keyword, resolution = 'weekly', start = 2019, end = 2023):
+    #TODO: check this function after replacing loop 
+    
+    not_keyword_indices = [] #TODO: do this without a loop (create extra boolean column and assign True if keyword is there)
+    for i, row in enumerate(df_clean['keywordStrings']):
+        if keyword not in row:
+            not_keyword_indices.append(i)
+
+    df_clean = df_clean.drop(not_keyword_indices)
+    df_clean['datetimes']= pd.to_datetime(df_clean['lastModifiedDate'])
+    df_clean['yearweek'] = df_clean['datetimes'].apply(lambda x: str(x.year)+str(x.week))
+
+    all_weeks = [str(year)+str(week).zfill(2) for week in range(1, 53) for year in range(start, end)] #TODO is *100 here necessary?
+    not_in_df = list(set(all_weeks) - set(df_clean['yearweek'].tolist()))
+    dw_mentions = dict(Counter(df_clean['yearweek'].tolist()))
+    for key_ in not_in_df:
+        dw_mentions[key_] = 0 
+    
+    df_dw_mentions = pd.DataFrame.from_dict(dw_mentions, orient='index', columns=['val'])
+    df_dw_mentions['week_str'] = [str(i) for i in df_dw_mentions.index]
+    df_dw_mentions = df_dw_mentions.sort_values(by='week_str')
+
+    return df_dw_mentions
+
+
 
 @click.group()
 def cli():
     pass
 
-@click.command(name='dw-kw-to-google')
+@click.command(name='extract-google-trends')
 @click.argument('db_path', type=click.Path(exists=True))
-@click.argument('kw_path', type=click.Path(exists=True))
+@click.option('kw_path', type=click.Path(exists=True))
 @click.option('--output', '-o', default=None)
 @click.option('--overwrite', default=False)
-def cli_dw_kw_to_google(db_path=None,
+def cli_extract_google_trends(db_path=None,
                         kw_path=None,
                         output=None,
                         overwrite=False
                         ):
-
     '''This function takes in the DW keywords and iterativeely 
     searchs for them for historical data on the Google API
+    then outputs a new df containing all of them 
     
-    Need to select an appropriate timescale to compare
-    Need to select appropriate temporal resolution 
+    Need to select appropriate temporal resolution. for this amount of time, google gives weekly searches by default
     '''
     if kw_path == None or overwrite:
         #let's load entire dataset and create keyword list from scratch 
@@ -159,26 +183,81 @@ def cli_dw_kw_to_google(db_path=None,
     #make keywords into df and use .apply to get google dicts 
     kw_df = pd.DataFrame({'keywords':keywords})
     kw_df['google_trends'] = kw_df['keywords'].apply(get_interest_over_time, end_date=('2023-01-01'))
-
-    #TODO: decide if you will make many dataframes inside original df or one big one with dates an index- thee second is easy to get from the first one
+    #TODO: decide if you will make many dataframes inside original df or one big one with dates an index- 
+    # thee second is easy to get from the first one
     # e.g.: new_big_df = pd.DataFrame({kw_df['keywords']: kw_df['google_trends']})
     
-
-
+    print('DFs with google searches saved at: ', output+'/'+google_kw_file_name)
     google_kw_file_name = 'dw_keywords_google_searches.npz'
     np.savez(op.join(output, google_kw_file_name), kw_df)
 
+cli.add_command(cli_extract_google_trends)
 
 
-#TODO: WRITEE MORE MAIN FUNCTIONS TO:
-#TODO: implement time series extraction from DW data 
 
-#TODO: decide if you will impose a limit on how many mentions and above you will create timeseries
+@click.command(name='dw-vs-google')
+@click.argument('db_path', type=click.Path(exists=True))
+@click.argument('kw_google_path', type=click.Path(exists=True))
+@click.option('--output', '-o', default=None)
+@click.option('--overwrite', default=False)
+def cli_dw_vs_google(db_path=None, # need this to make the timeseries
+                        kw_google_path=None, # need this to get the google searches 
+                        output=None,
+                        overwrite=False
+                        ):
+    '''
+    This function takes in the DW keywords 
+    creates a timeseries of mentions across time for them 
+    and compares them to google searches also extracted 
+    
+    Need to select an appropriate timescale to compare?
+    Need to select/develop multiple metrics to explore
+    Need to develop creative visualization methods 
+    '''
+    #let's load dataset: this should be 
+    df = load_data(db_path, keep=['id', 'keywordStrings', 'lastModifiedDate'])
+    df = truncate_data(df) #let's keep entiree dataset for now?
+    clean_df = clean_dataset(df) # is this actually possible??
 
-#TODO: compare time series etc.
+    #let's load and loop over unique cleaned keywords:
+    kw_google_df = np.load(kw_google_path)
+    for i, (keyword, google_searches) in enumerate(zip(kw_google_df['keywords'], kw_google_df['google_trends'])):
+        #TODO: reemove this loop if we can? maybe not                                  
+        print(keyword)
+        #now let's use this word on the original dataset to get time series
+        dw_mentions  = get_dw_timeseries(clean_df, keyword)
+
+        assert dw_mentions.shape[0] == google_searches.values.shape[0]
+        #TODO: we need a perfect way for matching dates between the two
+        # at the moment we have year+no_of_week in dw and actual date on google 01.01.19, 08.01.2019  etc.)
+
+        #now let's compare for this keyword:
+        # 1) Granger 'Causality': do dw articles follow closely after google searches
+        google_searches = google_searches.values
+
+        mix_df = pd.DataFrame({'dw':dw, 'google':google})
+        gc_res = grangercausalitytests(mix_df, 5)
+
+        # TODO: find any significant results and print/ report 
+
+        # TODO: implement more metrics to compare 
+
+        # TODO: plot graphs and metrics in a pdf?
+
+
+
+
+
+    #TODO: decide if you will impose a limit on how many mentions and above you will create timeseries
+
+
 
     
-cli.add_command(cli_dw_to_google)
+cli.add_command(cli_dw_vs_google)
+
+
+
+
 
 
 
