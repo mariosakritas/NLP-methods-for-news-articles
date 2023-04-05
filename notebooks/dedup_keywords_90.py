50e1f6949d0fabda65731319d604b01701dbcb9e
Wojciech Lason <40389067+wlason@users.noreply.github.com>
2023-04-05 10:11:26 +0100

    Delete dedup_keywords_90.py

--- notebooks/dedup_keywords_90.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from thefuzz import process
-
-
-uni_kw=pd.read_csv('data/interim/out_2019-2021_keywords_before_FuzzyWuzzy.csv')
-unique_keywords=set(uni_kw['0'])
-ded_kw=process.dedupe(unique_keywords, threshold = 90)
-
-pd.Series(list(ded_kw)).to_csv('data/interim/out_dedupl_90.csv')
